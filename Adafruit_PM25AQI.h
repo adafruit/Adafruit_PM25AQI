49ee7126221362894988a6861dfc4eaa3e33871d
Stephen Tierney <sjtrny@gmail.com>
2021-01-17 12:33:43 +1100

    Correct struct documentation so that it matches data sheet

--- Adafruit_PM25AQI.h
+++ Adafruit_PM25AQI.h
@@ -35,12 +35,12 @@ typedef struct PMSAQIdata {
   uint16_t pm10_env,       ///< Environmental PM1.0
       pm25_env,            ///< Environmental PM2.5
       pm100_env;           ///< Environmental PM10.0
-  uint16_t particles_03um, ///< 0.3um Particle Count
-      particles_05um,      ///< 0.5um Particle Count
-      particles_10um,      ///< 1.0um Particle Count
-      particles_25um,      ///< 2.5um Particle Count
-      particles_50um,      ///< 5.0um Particle Count
-      particles_100um;     ///< 10.0um Particle Count
+  uint16_t particles_03um, ///> 0.3um Particle Count
+      particles_05um,      ///> 0.5um Particle Count
+      particles_10um,      ///> 1.0um Particle Count
+      particles_25um,      ///> 2.5um Particle Count
+      particles_50um,      ///> 5.0um Particle Count
+      particles_100um;     ///> 10.0um Particle Count
   uint16_t unused;         ///< Unused
   uint16_t checksum;       ///< Packet checksum
 } PM25_AQI_Data;
