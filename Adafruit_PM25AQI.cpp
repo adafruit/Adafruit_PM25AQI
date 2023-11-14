91bb66678f476927f96277ed47f78f423852e85e
brentru <brent@adafruit.com>
2023-11-14 15:17:35 -0500

    add compatability and detection for Cubic PM1006 sensor

--- Adafruit_PM25AQI.cpp
+++ Adafruit_PM25AQI.cpp
@@ -70,6 +70,8 @@ bool Adafruit_PM25AQI::begin_UART(Stream *theSerial) {
 bool Adafruit_PM25AQI::read(PM25_AQI_Data *data) {
   uint8_t buffer[32];
   uint16_t sum = 0;
+  bool is_pm1006 = false;
+  uint8_t bufLen = 32;
 
   if (!data) {
     return false;
@@ -84,48 +86,86 @@ bool Adafruit_PM25AQI::read(PM25_AQI_Data *data) {
       return false;
     }
     int skipped = 0;
-    while ((skipped < 32) && (serial_dev->peek() != 0x42)) {
+    // Find the start character in the stream (0x42 for Adafruit PM sensors,
+    // 0x16 for the Cubic PM1006)
+    while ((skipped < 32) && (serial_dev->peek() != 0x42) &&
+           (serial_dev->peek != 0x16)) {
       serial_dev->read();
       skipped++;
       if (!serial_dev->available()) {
         return false;
       }
     }
-    if (serial_dev->peek() != 0x42) {
+    // Check for the start character in the stream
+    if ((serial_dev->peek() != 0x42) || (serial_dev->peek() != 0x16)) {
       serial_dev->read();
-      return false;
+      return false; // We did not find the start character
     }
-    // Now read all 32 bytes
-    if (serial_dev->available() < 32) {
-      return false;
+    // Are we using the PM1006?
+    if (serial_dev->peek() == 0x16) {
+      is_pm1006 = true;
     }
-    serial_dev->readBytes(buffer, 32);
+
+    // TODO: Can we do the check above in here instead to optimize?
+    if (!is_pm1006) {
+      // Start character was found, now read all 32 bytes
+      if (serial_dev->available() < 32) {
+        return false;
+      }
+      serial_dev->readBytes(buffer, 32);
+    } else {
+      // Start character was found, now read 20 bytes
+      // Note: PM1006 only requires 20 bytes, but we will ignore the last 12
+      // bytes when parsing
+      if (serial_dev->available() < 20) {
+        return false;
+      }
+      serial_dev->readBytes(buffer, 20);
+    }
   } else {
     return false;
   }
 
-  // Check that start byte is correct!
-  if (buffer[0] != 0x42) {
+  // Validate start byte is correct for Adafruit PM sensors
+  if ((!is_pm1006 && buffer[0] != 0x42)) {
     return false;
   }
 
-  // get checksum ready
-  for (uint8_t i = 0; i < 30; i++) {
+  // Validate start header is correct for Cubic PM1006
+  if (is_pm1006 &&
+      (buffer[0] != 0x16 || buffer[1] != 0x11 || buffer[2] != 0x0B)) {
+    return false;
+  }
+
+  // TODO: Could we make this better?
+  if (is_pm1006) {
+    bufLen = 20;
+  }
+
+  // Calculate the checksum
+  for (uint8_t i = 0; i < bufLen; i++) {
     sum += buffer[i];
   }
-
-  // The data comes in endian'd, this solves it so it works on all platforms
-  uint16_t buffer_u16[15];
-  for (uint8_t i = 0; i < 15; i++) {
-    buffer_u16[i] = buffer[2 + i * 2 + 1];
-    buffer_u16[i] += (buffer[2 + i * 2] << 8);
+  // Validate checksum
+  if ((is_pm1006 && sum != 0) || (!is_pm1006 && sum != data->checksum)) {
+    return false; // Invalid checksum
   }
 
-  // put it into a nice struct :)
-  memcpy((void *)data, (void *)buffer_u16, 30);
-
-  if (sum != data->checksum) {
-    return false;
+  // Since header and checksum are OK, parse data from the buffer
+  if (!is_pm1006) {
+    // The data comes in endian'd, this solves it so it works on all platforms
+    uint16_t buffer_u16[15];
+    for (uint8_t i = 0; i < 15; i++) {
+      buffer_u16[i] = buffer[2 + i * 2 + 1];
+      buffer_u16[i] += (buffer[2 + i * 2] << 8);
+    }
+    // put it into a nice struct :)
+    memcpy((void *)data, (void *)buffer_u16, 30);
+  } else {
+    // Parse from PM1006
+    // NOTE: PM1006 only produces a pm25_env reading!
+    data.pm25_env = (serialRxBuf[5] << 8) | serialRxBuf[6];
+    data.checksum = sum;
   }
 
   // success!
