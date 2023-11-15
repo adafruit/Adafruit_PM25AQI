1deff2a8baf9c7ad24c9591476647a33443dc976
brentru <brent@adafruit.com>
2023-11-15 17:09:51 -0500

    clang

--- Adafruit_PM25AQI.cpp
+++ Adafruit_PM25AQI.cpp
@@ -10,7 +10,7 @@
  * Adafruit PM2.5 Air quality sensors: http://www.adafruit.com/products/4632
  *
  * This library also works with the Cubic PM1006 UART Air Quality Sensor.
- * 
+ *
  * These sensors use I2C or UART to communicate.
  *
  * Adafruit invests time and resources providing this open source code,
@@ -20,8 +20,9 @@
  *
  * @section author Author
  * Written by Ladyada for Adafruit Industries.
- * Modified by Brent Rubell for Adafruit Industries for use with Cubic PM1006 Air Quality Sensor.
- * 
+ * Modified by Brent Rubell for Adafruit Industries for use with Cubic PM1006
+ * Air Quality Sensor.
+ *
  * @section license License
  * BSD license, all text here must be included in any redistribution.
  *
